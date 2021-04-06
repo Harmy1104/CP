651b8d50dffcb240b5bade261c42315bec123f87
Harmee Patel <harmeepate@gmail.com>
2021-04-06 22:06:53 +0530

    completing domino pilling

--- CodeForces/dominoPilling.cpp
+++ CodeForces/dominoPilling.cpp
@@ -4,52 +4,17 @@ using namespace std;
 #define ar array
 #define ll long long
 
-/*
-2,2 = 2
-4,4 = 8
-6,6 = 18
-
-3,4 = 6
-4,3 = 6
-
-3,2+1 = 3 + 1
-3,3 = 4
-5,5	= 12	
-3,7
-*/
-
-int rect(float m, float n){
-	if(fmod(m,2) != 0){
-		return ceil(n/2)*m;
-	} 
-	return ceil(m/2)*n;
-}
-
 void solve() {
 	// float w, h;
 	int w, h, ans = 0;
 	cin >> w >> h;
 	if(w%2 != 0)
-		ans += (w-1)/2;
-	for(int i = 0; i <= w; i+=2){
-		for(int j = 0; j <= h; j++){
-			ans ++;
-		}
+		ans += h%2 == 0 ? h/2 : (h-1)/2;
+	while(w/2 > 0 && w > 1){
+		ans += h;
+		w -= 2;
 	}
-	// if (fmod(w,2) == 0 || fmod(h,2) == 0){
-// 		cout << (w/2)*h;
-// 	}
-// 	else {
-// 		if (fmod(w,2) != 0 && fmod(h,2) != 0){
-// 			cout << rect(w-1, h) + ;
-// 		}
-// 		else if (fmod(w,2) != 0){
-// 			cout << ceil(h/2)*w;
-// 		}
-// 		else if (fmod(h,2) != 0){
-// 			cout << ceil(w/2)*h;
-// 		}
-// 	}
+	cout << ans;
 }
 
 int main() {
