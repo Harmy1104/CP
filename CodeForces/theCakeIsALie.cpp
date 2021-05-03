426cde9b1ccef1366dcfac91a1ba8ea801c79cb0
Harmee Patel <harmeepate@gmail.com>
2021-05-03 16:56:22 +0530

    03052021

---
+++ CodeForces/theCakeIsALie.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ar array
+#define ll long long
+#define pb push_back
+#define to_upper(a) transform((a).begin(), (a).end(), (a).begin(), ::toupper)
+#define to_lower(a) transform((a).begin(), (a).end(), (a).begin(), ::tolower)
+
+// . . . . .
+// . . . . .
+// . . . . .
+// . . . . .
+
+void solve() {
+	int n, m, k, y = 1;
+	cin >> n >> m >> k;
+	k -= m-1;
+	y += m-1;
+	k -= (n-1)*y;
+	k == 0 ? cout << "YES" << "\n" : cout << "NO" << "\n";
+}
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+    int tc = 1;
+	cin >> tc;
+    while(tc--) {
+        solve();
+    }
+}
